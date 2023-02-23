277f943c0c382f57b7a5f337f10eb39d3507dcb3
DDatj <jsmsj04@gmail.com>
2023-02-23 11:07:19 +0700

    Design Database

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,6 +2,7 @@ import { MongoClient } from 'mongodb';
 import { env } from './environment';
 
 const uri = env.MONGO_URI;
+let dbInstance = null;
 
 export const connectDB = async () => {
    const client = new MongoClient(uri, {
@@ -13,16 +14,21 @@ export const connectDB = async () => {
       await client.connect();
       console.log('Connected to database');
 
-      await listDatabases(client);
-
+      dbInstance = client.db(env.DATABASE_NAME);
    } catch (err) {
       console.log(err);
       process.exit(1);
    }
 };
 
-const listDatabases = async client => {
-   const databasesList = await client.db().admin().listDatabases();
-   console.log('Databases:');
-   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// Get database instance
+export const getDB = () => {
+   if (!dbInstance) throw new Error('Database not connected');
+   return dbInstance;
 };
+
+// const listDatabases = async client => {
+//    const databasesList = await client.db().admin().listDatabases();
+//    console.log('Databases:');
+//    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import { connectDB } from './config/mongodb';
-import { env } from './config/environment';
+import { env } from '*/config/environment';
 
-const app = express();
 const hostname = env.HOSTNAME;
 const port = env.PORT;
 
-connectDB();
+connectDB()
+   .then(() => bootServer())
+   .catch(err => {
+      console.log(err);
+      process.exit(1);
+   });
 
-app.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
-});
+const bootServer = () => {
+   const app = express();
+
+   app.get('/test', async (req, res) => {
+
+   });
+
+   app.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}/`);
+   });
+};
